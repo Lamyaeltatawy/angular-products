b7f4fa8137474c05eab8b6d53152ccf194f2b9a2
lamya <lamyaeltatawy@gmail.com>
2023-08-31 11:06:41 +0300

    products-edit

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-  constructor(private http:HttpClient) { }
+  name: string = 'ProductsService';
+  constructor(private http: HttpClient) {}
 
   getAllProducts() {
-return this.http.get('https://fakestoreapi.com/products')
+    return this.http.get('https://fakestoreapi.com/products');
   }
-  
 }
